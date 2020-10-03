f86424d6193b3a2a41f3292522451390e20924e7
Obinna Okafor <okaforobinna7@gmail.com>
2020-10-03 23:20:30 +0100

    Updates

---
+++ precache-manifest.943fc63c5a050cb89a427cedae422474.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "263292855108bc4bc28ed2b262eafba0",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "7f2e5856dfb9c4c992ff",
+    "url": "/markdown-previewer/static/css/main.095bb12c.chunk.css"
+  },
+  {
+    "revision": "1c78f2d0925c664aac0d",
+    "url": "/markdown-previewer/static/js/2.afe9b6db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/markdown-previewer/static/js/2.afe9b6db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f2e5856dfb9c4c992ff",
+    "url": "/markdown-previewer/static/js/main.722c5ab8.chunk.js"
+  },
+  {
+    "revision": "380a9cf99d31db8da525",
+    "url": "/markdown-previewer/static/js/runtime-main.4fd0b226.js"
+  },
+  {
+    "revision": "987328599ba9bebdda099fb24e81d917",
+    "url": "/markdown-previewer/static/media/Initialmarkdown.98732859.md"
+  }
+]);
